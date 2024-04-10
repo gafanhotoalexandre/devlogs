e93662dfd1167ab004c0c9c93136a1b66358d94a
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-09 21:32:58 -0300

    validacoes para Post schema

--- sanity/schemas/post.ts
+++ sanity/schemas/post.ts
@@ -1,3 +1,5 @@
+import { Rule } from 'sanity'
+
 export const post = {
   name: 'post',
   title: 'Post',
@@ -8,22 +10,27 @@ export const post = {
       name: 'title',
       title: 'Title',
       type: 'string',
+      validation: (Rule: Rule) => Rule.required().error('Campo obrigatório'),
     },
     {
       name: 'slug',
       title: 'slug',
       type: 'slug',
       options: { source: 'title' },
+      validation: (Rule: Rule) => Rule.required().error('Campo obrigatório'),
     },
     {
       name: 'publishedAt',
       title: 'Published at',
       type: 'datetime',
+      initialValue: () => new Date().toISOString(),
     },
     {
       name: 'excerpt',
       title: 'Excerpt',
       type: 'text',
+      validation: (Rule: Rule) =>
+        Rule.max(200).error('Máximo de 200 caracteres.'),
     },
     {
       name: 'body',
